ccc43fd5f113b6237df7746e0734e2b300173e70
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-08-06 18:41:09 -0400

    Initial commit

---
+++ default.nix
@@ -0,0 +1,31 @@
+{
+  buildNpmPackage,
+  fetchFromGitHub,
+  lib,
+}:
+buildNpmPackage rec {
+  pname = "mailsy";
+  version = "5.0.0";
+
+  src = fetchFromGitHub {
+    owner = "BalliAsghar";
+    repo = "Mailsy";
+    rev = version;
+    hash = "sha256-RnOWvu023SOcN83xEEkYFwgDasOmkMwSzJ/QYjvTBDo=";
+  };
+
+  npmDepsHash = "sha256-ljmqNmLvRHPdsKyOdDfECBXHTIExM6nPZF45lqV+pDM=";
+
+  npmFlags = ["--legacy-peer-deps"];
+
+  dontNpmBuild = true;
+
+  meta = with lib; {
+    description = "Check for outdated, incorrect, and unused dependencies";
+    mainProgram = "npm-check";
+    homepage = "https://github.com/dylang/npm-check";
+    changelog = "https://github.com/dylang/npm-check/releases/tag/v${version}";
+    license = licenses.mit;
+    maintainers = [maintainers.thomasjm];
+  };
+}
